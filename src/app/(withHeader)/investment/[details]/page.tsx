08676d846e0adf621a813e69d24c8ce01631295a
Zayed Hossain <zayedhossain120@gmail.com>
2024-01-08 12:12:24 +0600

    trying to getting the id

---
+++ src/app/(withHeader)/investment/[details]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DetailsPage = ({ params }: any) => {
+  console.log(params.details.id, "i got the id");
+
+  return <div></div>;
+};
+
+export default DetailsPage;
