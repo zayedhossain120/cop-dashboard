bc62a9e9ae014996076419f8d215ea1aa5cd793d
Jaker Hossain <mrporanyt120@gmail.com>
2024-01-10 12:41:46 +0600

    working on deploying and authentication

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -7,6 +7,44 @@ import bcryptjs from "bcryptjs";
 connect();
 export async function POST(request: NextRequest) {
   try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    //check if users exits
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "user not found" }, { status: 404 });
+    }
+    console.log("user exits");
+
+    //check if password is correct
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "invalid password" }, { status: 401 });
+    }
+    console.log("user", user);
+
+    //get token data
+    const tokenData = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    //create token
+    const token = jwt.sign(tokenData, process.env.NODE_ENV, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      succuss: true,
+    });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+    return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
