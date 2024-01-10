7871ea1000c006fbf11cb42b8568e5d29fd202d3
Zayed Hossain <zayedhossain120@gmail.com>
2024-01-10 19:55:19 +0600

    server port changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverOptions: {
+    port: 3001,
+  },
   images: {
-    domains: ["res.cloudinary.com", "images.pexels.com"],
+    domains: [
+      "res.cloudinary.com",
+      "images.pexels.com",
+      "images.remotePatterns",
+    ],
   },
 };
 
